a2e4a6a4d1834cd23c1ece13e31278cca6823879
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-05-04 22:24:32 +0900

    Feat: JPQL 실습

--- src/main/java/umc8th/spring8th/repository/MemberRepository.java
+++ src/main/java/umc8th/spring8th/repository/MemberRepository.java
@@ -1,9 +1,21 @@
 package umc8th.spring8th.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import umc8th.spring8th.domain.Member;
+import umc8th.spring8th.domain.enums.MemberStatus;
+
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // 메서드 이름으로 쿼리 생성
+//    List<Member> findByNameAndStatus(String name, MemberStatus status);
+
+  // @Query 어노테이션으로 JPAL 직접 작성
+    @Query("SELECT m FROM Member m WHERE m.name = :name AND m.status = :status")
+    List<Member> findByNameAndStatus(@Param("name") String name, @Param("status") MemberStatus status);
 }
