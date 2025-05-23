1ca4284f6c2cdbde93aafafc91a274d5a52b076d
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-05-24 05:02:40 +0900

    Feat: repository 작성

--- src/main/java/umc8th/spring8th/repository/MissionRepository/MissionRepository.java
+++ src/main/java/umc8th/spring8th/repository/MissionRepository/MissionRepository.java
@@ -1,7 +1,12 @@
 package umc8th.spring8th.repository.MissionRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc8th.spring8th.domain.Mission;
+import umc8th.spring8th.domain.Store;
 
 public interface MissionRepository extends JpaRepository<Mission, Long>, MissionRepositoryCustom {
+
+    Page<Mission> findAllByStore(Store store, PageRequest pageRequest);
 }
